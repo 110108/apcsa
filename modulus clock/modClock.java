577b5895d0cca4b6dce152af15bf3f4e0fb1855d
110108 <31996737+110108@users.noreply.github.com>
2018-09-05 10:15:08 -0400

    work

---
+++ modulus clock/modClock.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class modClock
+{
+
+	public static void main(String[] args)
+	{
+		int sHour;
+		int pHour
+		int eHour;
+		int sMin;
+		int pMin;
+		int eMin;
+		Scanner sc=new Scanner(System.in);
+
+
+	}
+}
+
