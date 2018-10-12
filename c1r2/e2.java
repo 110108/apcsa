eb2ae87b1ebfe6b02160719a0547576c4d1552dd
110108 <31996737+110108@users.noreply.github.com>
2018-10-12 08:06:55 -0400

    e2

---
+++ c1r2/e2.java
@@ -0,0 +1,16 @@
+/*
+•• E1.2Write a program that prints the sum of the first ten positive integers, 1 + 2 + … + 10.
+*/
+import java.util.Scanner;
+
+public class e2
+{
+
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("1+2+3+4+5+6+7+8+9+10="+(1+2+3+4+5+6+7+8+9+10));
+	}
+}
+
