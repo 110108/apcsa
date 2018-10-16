ccc3fc3245f1a4c6503ee7df3d2f67b9ffed53e1
110108 <31996737+110108@users.noreply.github.com>
2018-10-16 07:38:37 -0400

    c1r2 finished

--- c1r2/Test.java
+++ c1r2/Test.java
@@ -6,8 +6,8 @@ public class Test
    public static void main(String[] args) throws Exception
    {
       URL imageLocation = new URL(
-            "http://horstmann.com/java4everyone/duke.gif");
-      JOptionPane.showMessageDialog(null, "Hello", "Title",
+            "http://slavicstyle.com/wp-content/uploads/2015/03/13911588143753.jpg");
+      JOptionPane.showMessageDialog(null, "U BRING CIGARET", "Title",
             JOptionPane.PLAIN_MESSAGE, new ImageIcon(imageLocation));
    }
 }
--- c1r2/e17.java
+++ c1r2/e17.java
@@ -1,9 +1,11 @@
 import javax.swing.JOptionPane;
-public class DialogViewer
+public class e17
 {
    public static void main(String[] args)
    {
       String name = JOptionPane.showInputDialog("What is your name?");
       JOptionPane.showInputDialog("Hello, "+name+"!");
+      String op = JOptionPane.showInputDialog("What woluld you like me to do?");
+      System.out.println("I'm sorry "+name+", but I can't do that.");
    }
 }
