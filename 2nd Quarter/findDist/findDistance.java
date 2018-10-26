a0acdc50dd35d725243a08560233687878630f4a
110108 <31996737+110108@users.noreply.github.com>
2018-10-26 13:35:33 -0400

    update the blat

---
+++ 2nd Quarter/findDist/findDistance.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class findDistance
+{
+   public static void main(String[] args) throws Exception
+   {
+      Scanner sc = new Scanner(System.in);
+      int x0;
+      int x1;
+      int x2;
+      int x3;
+      int y0;
+      int y1;
+      int y2;
+      int y3;
+
+      for(int i; i <= 3; i++){
+        if(i==0){System.out.println("\nThe house:");}
+        if(i==1){System.out.println("\nErrand 1:");}
+        if(i==2){System.out.println("\nErrand 2:");}
+        if(i==3){System.out.println("\nErrand 3:");}
+        System.out.print("Enter x coodinate: ");
+        x = sc.nextInt();
+        System.out.print("Enter y coodinate: ");
+        y = sc.nextInt();
+      }
+      //post loop code
+    }
+  }
