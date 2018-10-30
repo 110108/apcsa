c5b3fe64e51fc648679e561dbbe372f2a792b717
110108 <31996737+110108@users.noreply.github.com>
2018-10-30 08:15:31 -0400

    FINALLY finished find dist

--- 2nd Quarter/findDist/findDistance.java
+++ 2nd Quarter/findDist/findDistance.java
@@ -30,7 +30,7 @@ public class findDistance
 		  System.out.print("Enter y coodinate: ");
 		  y = sc.nextInt();
 		  if(i==0){
-			  x0x;
+			  x0=x;
 			  y0=y;
 		  }
 		  if(i==1){
@@ -48,7 +48,16 @@ public class findDistance
 	  }
 
 	  //math
-	  d1 = math.sqrt(math.pow((x1-x0),2) + math.pow((y1-y0),2));
-	  System.out.println(d1);
+	  d1 = Math.sqrt(Math.pow((x1-x0),2) + Math.pow((y1-y0),2));
+	  d2 = Math.sqrt(Math.pow((x2-x0),2) + Math.pow((y2-y0),2));
+	  d3 = Math.sqrt(Math.pow((x3-x0),2) + Math.pow((y3-y0),2));
+
+	  //return
+	  System.out.println("\nDistance to Errand one: " + d1);
+	  System.out.println("Distance to Errand two: " + d2);
+	  System.out.println("Distance to Errand three: " + d3 + "\n");
+	  if(d1 < d2 && d1 < d3){System.out.println("Errand one is closest.");}
+	  if(d2 < d1 && d2 < d3){System.out.println("Errand two is closest.");}
+	  if(d3 < d1 && d3 < d2){System.out.println("Errand three is closest.");}
    }
 }
