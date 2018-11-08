abb3e6b7e60e35e869bc92153212033d67cdaa53
110108 <31996737+110108@users.noreply.github.com>
2018-11-08 07:59:59 -0500

    stuff hapened

--- 2nd Quarter/fourDigitRomNum/fourDigitRomNum.java
+++ 2nd Quarter/fourDigitRomNum/fourDigitRomNum.java
@@ -8,15 +8,20 @@ public class fourDigitRomNum
       String rom = "";
       int num;
 
-      System.out.println("Please enter the number: ");
+      System.out.print("Please enter a number between 0 and 4000: ");
       num = sc.nextInt();
+
+      if(num>=4000||num<=0){
+		  System.out.println("\nNumber out of range\n");
+		  System.exit(0);
+	  }
 
       do{
         if(num>=1000){
           num-=1000;
           rom+="M";
           continue;
-        }
+	  }
         if(num>=500){
           num-=500;
           rom+="D";
@@ -37,16 +42,27 @@ public class fourDigitRomNum
           rom+="X";
           continue;
         }
+         if(num>=9){
+			 num-=9;
+			 rom+="IX";
+			 continue;
+        }
         if(num>=5){
           num-=5;
           rom+="V";
           continue;
+        }
+         if(num>=4){
+			 num-=4;
+			 rom+="IV";
+			 continue;
         }
         if(num>=1){
           num-=1;
           rom+="I";
           continue;
         }
-      }
+      }while(num > 0);
+      System.out.println(rom);
    }
 }
