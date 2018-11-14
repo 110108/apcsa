201745bbd2b3eb98073cf4db0a8a57d98a05ea8f
110108 <31996737+110108@users.noreply.github.com>
2018-11-14 10:28:48 -0500

    summary

---
+++ 2nd Quarter/R6_4/R6_4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class R6_4
+{
+   public static void main(String[] args) throws Exception
+   {
+      Scanner sc = new Scanner(System.in);
+      int in;
+
+      System.out.print("Gimmie a positive integer: ");
+      in = sc.nextInt();
+
+      while(/*code*/)
+      {
+		  /*morecode*/
+	  }
+
+	  while(/*code*/)
+	  {
+		  /*morecode*/
+	  }
+   }
+}
---
+++ 2nd Quarter/primeGenerator/PrimeGenerator.java
@@ -0,0 +1,29 @@
+/**
+   This class generates all prime numbers.
+*/
+public class PrimeGenerator
+{
+
+
+   public PrimeGenerator()
+   {
+	   //
+   }
+
+   /**
+      Calculates the next prime number.
+      @return the next prime number
+   */
+   public int nextPrime()
+   {
+      return 0;
+   }
+   /**
+	 Checks to see if number is prime.
+	 @return true or false
+   */
+   public static boolean isPrime(int n)
+   {
+      return true;
+   }
+}
