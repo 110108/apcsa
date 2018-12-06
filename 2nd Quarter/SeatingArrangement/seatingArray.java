e8138768a390524d85b89e7e87c89c826a4136ed
110108 <31996737+110108@users.noreply.github.com>
2018-12-06 10:01:52 -0500

    Summary

--- 2nd Quarter/SeatingArrangement/seatingArray.java
+++ 2nd Quarter/SeatingArrangement/seatingArray.java
@@ -20,6 +20,7 @@ use first distanced seat
 */
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 
 public class seatingArray
 {
@@ -32,35 +33,44 @@ public class seatingArray
       int nextSeat = -1;
       boolean[] seating;
 
+      //defining the array
       System.out.print("please enter the number of seats: ");
       while(!sc.hasNextInt()){
-		  System.out.print("please enter a positive integer, that is greater than one: ");
-		  sc.nextLine();
-	  }
-	  seats = sc.nextInt();
+        System.out.print("please enter a positive integer, that is greater than one: ");
+        sc.nextLine();
+      }
+      seats = sc.nextInt();
+      seating = new boolean[seats];
 
-	  seating = new boolean[seats];
-	  for(int i = 0; i < seating.length; i++){
-		  seating[i] = false;
-	  }
-	  System.out.println(seating.length);
+      //filling array with false just incase
+      for(int i = 0; i < seating.length; i++){
+        seating[i] = false;
+      }
+      //System.out.println(seating.length);
 
-	  do{
-		  for(int i = 0; i < seating.length; i++){
-		  	if(seating[i] == true){firstSeat = i;}
-		  	if(firstSeat==-1){
-				seat=firstSeat=((seating.length+1)/2);
-			  	break;
+      int count = 0;
+      do{
+        if(firstSeat==-1){
+			if(seating.length > 1){
+				seat=firstSeat=((seating.length)/2);
 			}
-			System.out.println(!true);
-			if((seating[i]==true)&&(firstSeat != -1)){nextSeat = i;}
-		}
-		seating[seat] = true;
-		System.out.print(seating);
-	}while(!seating[seating.length-1] == true);
-
-	  System.out.println(seat);
-	  System.out.println(firstSeat);
-	  System.out.println((seating.length+1)/2);
-   }
-}
+			else{
+				seat=0;
+			}
+          seating[seat] = true;
+          continue;
+        }
+        count++;
+        for(int i = 0; i < seating.length; i++){
+          if(seating[i] == true){firstSeat = i;}
+          if((seating[i]==true)&&(firstSeat != -1)){nextSeat = i;}
+        }
+        //System.out.println();
+        //System.out.println(seating.toString() + " " + count);
+      }while((!seating[seating.length-1] == true)&&(count < (seating.length * 2)));
+      //System.out.println(seat);
+      //System.out.println(firstSeat);
+      //System.out.println((seating.length+1)/2);
+      System.out.println(Arrays.toString(seating));
+    }
+  }
