3a1c3ef633948e1999ddfe0439a6da803642f459
110108 <31996737+110108@users.noreply.github.com>
2018-12-18 08:25:58 -0500

    worked on R6_5

---
+++ 2nd Quarter/R6_5/R6_5.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class R6_5
+{
+   public static void main(String[] args) throws Exception
+   {
+      Scanner sc = new Scanner(System.in);
+      int a;
+      int b;
+      int sumA = 0;
+      int squaresA = 0;
+      int sumB = 0;
+      int squaresB = 0;
+
+
+   }
+}
