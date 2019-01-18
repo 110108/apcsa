8275adb91b7ae0477644e199de86d21cba509eaf
110108 <31996737+110108@users.noreply.github.com>
2019-01-18 14:04:04 -0500

    array list work

---
+++ 3rd Quarter/ArrayList Review/ArrayList.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ArrayList
+{
+   public static void main(String[] args) throws Exception
+   {
+      Scanner sc = new Scanner(System.in);
+      ArrayList wl1;
+      ArrayList wl2;
+
+      while(true){
+		  System.out.print("Enter a word ('XXX' to quit): ");
+		  if(sc.nextLine().contains("xxx")){
+			  break;
+		  }
+		  //
+	  }
+
+   }
+}
