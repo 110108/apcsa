f7b96815512309a96b458fca4a33abb91ad6e420
110108 <31996737+110108@users.noreply.github.com>
2019-02-12 10:16:30 -0500

    worked

---
+++ 3rd Quarter/2d arrays/Arrays2D.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Arrays2D
+{
+   public static void main(String[] args) throws Exception
+   {
+	  File data = new File("C:\\Users\\110108\\Documents\\GitHub\\apcsa\\3rd Quarter\\2d arrays\\testData.txt");
+      Scanner sc = new Scanner(System.in);
+      Scanner file = new Scanner(data);
+   }
+}
