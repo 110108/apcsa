211115bd973b6d0f2628455e92ca649657be3242
110108 <31996737+110108@users.noreply.github.com>
2019-02-16 15:09:30 -0500

    finished 2d arrays

--- 3rd Quarter/2d arrays/Arrays2D.java
+++ 3rd Quarter/2d arrays/Arrays2D.java
@@ -1,16 +1,49 @@
-import java.util.Scanner;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.util.*;
+import java.io.*;
 
 public class Arrays2D
 {
    public static void main(String[] args) throws Exception
    {
-	  File data = new File("C:\\Users\\110108\\Documents\\GitHub\\apcsa\\3rd Quarter\\2d arrays\\testData.txt");
       Scanner sc = new Scanner(System.in);
-      Scanner in = new Scanner(data);
+      Scanner in = new Scanner(new File("C:\\Users\\Tech\\Documents\\GitHub\\apcsa\\3rd Quarter\\2d arrays\\testData.txt.txt"));
+      int rows=0;
+      int cols=0;
 
+      if(in.hasNextInt()){cols=in.nextInt();}
+      else
+      {
+		  System.out.println("Not a valid entry for columns");
+		  System.exit(0);
+	  }
+      if(in.hasNextInt()){rows=in.nextInt();}
+      else
+      {
+		  System.out.println("Not a valid entry for rows");
+		  System.exit(0);
+	  }
+      int[][] data = new int[rows][cols];
 
+      for(int i = 0; i<rows; i++)
+      {
+		  int count=0;
+		  for(int j = 0; j<cols; j++)
+		  {
+			  if(in.hasNextInt())
+			  {
+				  int temp=in.nextInt();
+				  count+=temp;
+				  data[i][j]=temp;
+			  }
+			  else
+			  {
+				  System.out.println("oops thats not an integer");
+				  System.exit(0);
+			  }
+		  }
+		  System.out.println("Average of row " + (i + 1) + ": " + (count / cols));
+	  }
+
+	  //System.out.println(Arrays.deepToString(data));
    }
 }
