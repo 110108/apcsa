8a77ee210d3879830bbcb55e47749cd5c6b20024
110108 <31996737+110108@users.noreply.github.com>
2019-04-01 09:53:01 -0400

    Update workDog.java

--- 4th Quarter/The Dog Class/workDog.java
+++ 4th Quarter/The Dog Class/workDog.java
@@ -1,22 +1,33 @@
 import java.util.Scanner;
 
-public class workDog
+public class workDog extends Dog
 {
 	private String occupation;
     private Boolean inTraining;
     private String trainer;
     private int yearsOfService;
 
-    public workDog(String job)
+    public workDog(String name,String job)
     {
+		super(name);
 		occupation = job;
 		inTraining = false;
 		trainer = null;
 		yearsOfService = 0;
 	}
 
-	public workDog(String job, String trainer)
+	public workDog(String name,String breed,String job)
+	    {
+			super(name,breed);
+			occupation = job;
+			inTraining = false;
+			trainer = null;
+			yearsOfService = 0;
+	}
+
+	public workDog(String name, String job, String trainer)
 	{
+		super(name);
 		occupation = job;
 		inTraining = false;
 		trainer = trainer;
@@ -25,6 +36,7 @@ public class workDog
 
 	public workDog(String job, Boolean training, String trainer, int YOS)
 	{
+		super();
 		occupation = job;
 		inTraining = training;
 		trainer = trainer;
