6899f9410d7383c58a9f14901f4402c7edf1056b
110108 <31996737+110108@users.noreply.github.com>
2019-04-01 07:47:56 -0400

    3rd quarter final

---
+++ 3rd Quarter/WritingToFile/05b.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/*Create and fill a 2x6 (2 columns, 6 rows) array with the first and last names of 6 fictional characters of your choice. Column 0 contains first names and column 1 contains last names. This can be done using one line of code (you do NOT need to use a for-loop to fill the array).
+Create a FileWriter object to open the txt file.
+Write "First and Last\n"
+Create a loop that will iterate through the rows. Write the row number followed by the first and last name and a line break.
+Write "Last names only\n"
+Create a loop that will iterate through the rows. Write the row number followed by the last name and a line break.
+Close the file.*/
+
+public class 05b
+{
+   public static void main(String[] args) throws Exception
+   {
+      Scanner sc = new Scanner(System.in);
+   }
+}
